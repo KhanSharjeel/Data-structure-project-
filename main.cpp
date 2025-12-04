a295fbadac3a19af61dca3ae366336ce312eb432
MrFaizan44 <malixfaizan445@gmail.com>
2025-12-04 23:22:11 +0500

    new change

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@ using namespace std;
 int main() {
     int number;
     cout << "Hello, World!" << endl;
-    cout << " How are you? " << endl;
-    cin>>number;
-    cout << "You entered: " << number << endl;
+    cout << " How are you " << endl;
+    
     return 0;
 }
