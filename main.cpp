f94b74c7bf3a708e9f0187817212c144abb5059e
MrFaizan44 <malixfaizan445@gmail.com>
2025-12-04 22:47:15 +0500

    new changing line

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main() {
+    int number;
     cout << "Hello, World!" << endl;
+    cout << " How are you? " << endl;
+    cin>>number;
+    cout << "You entered: " << number << endl;
     return 0;
 }
