66e0776f0a680fed16d3aa5804d0cf6a84c24333
MrFaizan44 <malixfaizan445@gmail.com>
2025-12-08 11:59:42 +0500

    faizan

---
+++ Linklist.h
@@ -0,0 +1,23 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include "Student.h"
+
+class Node {
+public:
+    Student data;
+    Node* next;
+
+    Node(Student s);
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList();
+    void insert(Student s);
+    void display();
+};
+
+#endif
