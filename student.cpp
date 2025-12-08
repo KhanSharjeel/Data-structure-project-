bb91682ffa4ec06973bc4b789e2f0018a7b02da9
KhanSharjeel <53099@studens.riphah.edu.pk>
2025-12-08 11:55:42 +0500

    time 11:55 am

---
+++ student.cpp
@@ -0,0 +1,29 @@
+#include "Student.h"
+#include <iostream>
+using namespace std;
+
+Student::Student() {
+    id = 0;
+    name = "";
+    age = 0;
+}
+
+Student::Student(int i, string n, int a) {
+    id = i;
+    name = n;
+    age = a;
+}
+
+void Student::input() {
+    cout << "Enter ID: ";
+    cin >> id;
+    cin.ignore();
+    cout << "Enter Name: ";
+    getline(cin, name);
+    cout << "Enter Age: ";
+    cin >> age;
+}
+
+void Student::display() {
+    cout << "ID: " << id << ", Name: " << name << ", Age: " << age << endl;
+}
