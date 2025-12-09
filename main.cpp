ec2d77e83763dc97b7c77a6446dd4b1614a6f331
MrFaizan44 <malixfaizan445@gmail.com>
2025-12-09 11:33:47 +0500

    Faizan Malik

--- main.cpp
+++ main.cpp
@@ -1,14 +1,53 @@
-#include<iostream>
+#include <iostream>
+#include "Student.h"
+#include "LinkList.h"
+#include "DoublyLinkedList.h"
+#include "BST.h"
+
 using namespace std;
 
 int main() {
-    int number;
-    cout << "Hello, World!" << endl;
-    cout << " How are you? " << endl;
-    cin>>number;
-    cout << "You entered: " << number << endl;
-    cout<<" This is C++17 standard code. "<<endl;
-    cout<<" Enjoy coding! "<<endl;
+    LinkedList list;
+    DoublyLinkedList dlist;
+    BST tree;
+
+    int choice;
+
+    while (true) {
+        cout << "\n1. Add Student\n2. Display Linked List\n3. Display Doubly Linked List\n4. Display BST (In-order)\n5. Exit\n";
+        cin >> choice;
+
+        if (choice == 1) {
+            Student s;
+            s.input();
+
+            list.insert(s);
+            dlist.insert(s);
+            tree.insertStudent(s);
+
+            cout << "Student Added!\n";
+        }
+
+        else if (choice == 2) {
+            list.display();
+        }
+
+        else if (choice == 3) {
+            dlist.displayForward();
+        }
+
+        else if (choice == 4) {
+            tree.inOrder(tree.root);
+        }
+
+        else if (choice == 5) {
+            break;
+        }
+
+        else {
+            cout << "Invalid choice!\n";
+        }
+    }
+
     return 0;
 }
-
